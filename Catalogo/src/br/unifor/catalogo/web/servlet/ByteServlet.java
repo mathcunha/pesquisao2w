28c4fe45e1f2308293b805ca1c07e4e7bdbb4297
mathcunha <mathcunha@81f30078-14c3-11de-9308-ab3afd9c0aba>
2009-07-21 19:56:19 +0000

    Testes isolados

--- Catalogo/src/br/unifor/catalogo/web/servlet/ByteServlet.java
+++ Catalogo/src/br/unifor/catalogo/web/servlet/ByteServlet.java
@@ -9,6 +9,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -17,12 +18,15 @@ import javax.servlet.http.HttpServletResponse;
 import br.unifor.catalogo.persistence.CatalogoTO;
 import br.unifor.catalogo.persistence.FilmeTO;
 import br.unifor.catalogo.persistence.manager.PersistenceDelegate;
+import br.unifor.catalogo.test.TestCatalogo;
 
 /**
  * Servlet implementation class ByteServlet
  */
 public class ByteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+
+	private List<CatalogoTO> filmes;
 
 	/**
 	 * @see HttpServlet#HttpServlet()
@@ -79,7 +83,7 @@ public class ByteServlet extends HttpServlet {
 		} else if ("excluir".equals(strAcao)) {
 			delegate.delete(filme);
 			listar(request, response, filme, delegate);
-		} else if ("exibir".equals(strAcao)) {			
+		} else if ("exibir".equals(strAcao)) {
 			filme.setIdentificador(delegate.findByPk(filme).getIdentificador());
 			request.setAttribute("bean", filme);
 			request.getRequestDispatcher("byte/editar.jsp").forward(request,
@@ -89,90 +93,125 @@ public class ByteServlet extends HttpServlet {
 			listar(request, response, filme, delegate);
 		} else if ("listar".equals(strAcao)) {
 			listar(request, response, filme, delegate);
+		} else if ("teste".equals(strAcao)) {
+			sequenciaTeste(new Integer(request.getParameter("qtd")),
+					new Integer(request.getParameter("desvio")));
+			listar(request, response, filme, delegate);
 		}
 
 	}
 
 	private void popular() throws SecurityException, IOException {
 		PersistenceDelegate delegate = PersistenceDelegate.getInstance();
+
+		for (CatalogoTO catalogo : filmes) {
+			delegate.insert(catalogo);
+		}
+
+	}
+
+	private void sequenciaTeste(int quantidade, int desvio) throws SecurityException, IOException{
+		PersistenceDelegate delegate = PersistenceDelegate.getInstance();
+		
+		List<CatalogoTO> lFilmes = new ArrayList<CatalogoTO>(quantidade +1);
+		CatalogoTO lCatalogo;
+		
+		for (int i = 0; i < quantidade; i++) {
+			lCatalogo = filmes.get(i);
+			
+			lCatalogo.setIdentificador(lCatalogo.getIdentificador() + desvio);
+			lFilmes.add(lCatalogo);
+		}
+		
+		TestCatalogo lTestCatalogo = new TestCatalogo();
+		lTestCatalogo.execute(delegate, lFilmes, desvio == 0);
+		
+	}
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+
+		filmes = new ArrayList<CatalogoTO>(30);
+
 		FilmeTO filme = new FilmeTO(1l, "X-Men", 30l);
-		delegate.insert(filme);
+		filmes.add(filme);
 
 		filme = new FilmeTO(2l, "X-Men 3", 30l);
-		delegate.insert(filme);
+		filmes.add(filme);
 
-		filme = new FilmeTO(3l, "Simonal – Ninguém sabe o duro que dei  ",
-				30l);
-		delegate.insert(filme);
+		filme = new FilmeTO(3l, "Simonal – Ninguém sabe o duro que dei  ", 30l);
+		filmes.add(filme);
 
 		filme = new FilmeTO(4l, "Palavra (en)cantada", 30l);
-		delegate.insert(filme);
+		filmes.add(filme);
 
 		filme = new FilmeTO(5l, "Uma Noite no Museu 2", 30l);
-		delegate.insert(filme);
+		filmes.add(filme);
 
 		filme = new FilmeTO(6l, "A Montanha Enfeitiçada", 30l);
-		delegate.insert(filme);
+		filmes.add(filme);
 
 		filme = new FilmeTO(7l, "X-Men Origens: Wolverine", 30l);
-		delegate.insert(filme);
+		filmes.add(filme);
 
 		filme = new FilmeTO(8l, "Anjos e Demônios", 30l);
-		delegate.insert(filme);
+		filmes.add(filme);
 
 		filme = new FilmeTO(9l, "Monstros vs Alienígenas", 30l);
-		delegate.insert(filme);
+		filmes.add(filme);
 
 		filme = new FilmeTO(10l, "A Festa do Garfield", 30l);
-		delegate.insert(filme);
+		filmes.add(filme);
 
 		filme = new FilmeTO(11l, "Star Trek", 30l);
-		delegate.insert(filme);
+		filmes.add(filme);
 
 		filme = new FilmeTO(12l, "Divã", 30l);
-		delegate.insert(filme);
+		filmes.add(filme);
 
 		filme = new FilmeTO(13l, "Noivas Em Guerra", 30l);
-		delegate.insert(filme);
+		filmes.add(filme);
 
 		filme = new FilmeTO(14l, "Lutador, O", 30l);
-		delegate.insert(filme);
+		filmes.add(filme);
 
 		filme = new FilmeTO(15l, "Sete Vidas", 30l);
-		delegate.insert(filme);
+		filmes.add(filme);
 
 		filme = new FilmeTO(16l, "Foi Apenas Um Sonho", 30l);
-		delegate.insert(filme);
+		filmes.add(filme);
 
 		filme = new FilmeTO(17l, "Se Eu Fosse Você", 30l);
-		delegate.insert(filme);
+		filmes.add(filme);
 
 		filme = new FilmeTO(18l, "Dúvida", 30l);
-		delegate.insert(filme);
+		filmes.add(filme);
 
 		filme = new FilmeTO(19l, "Quarentena", 30l);
-		delegate.insert(filme);
+		filmes.add(filme);
 
 		filme = new FilmeTO(20l, "Budapeste", 30l);
-		delegate.insert(filme);
+		filmes.add(filme);
 
 		filme = new FilmeTO(21l, "Velozes e Furiosos", 30l);
-		delegate.insert(filme);
+		filmes.add(filme);
 
 		filme = new FilmeTO(22l, "Desejo e Perigo", 30l);
-		delegate.insert(filme);
+		filmes.add(filme);
 
 		filme = new FilmeTO(23l, "O senhor do aneis", 30l);
-		delegate.insert(filme);
+		filmes.add(filme);
 
 		filme = new FilmeTO(24l, "loucademia de polícia", 30l);
-		delegate.insert(filme);
+		filmes.add(filme);
 
 		filme = new FilmeTO(25l, "Shrek", 30l);
-		delegate.insert(filme);
+		filmes.add(filme);
 
 		filme = new FilmeTO(26l, "Matrix", 30l);
-		delegate.insert(filme);
+		filmes.add(filme);
+
 	}
 
 	private void listar(HttpServletRequest request,
