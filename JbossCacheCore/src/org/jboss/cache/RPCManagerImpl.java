cbe476dccf4e06707c627dcc152018fac08faee7
mathcunha <mathcunha@81f30078-14c3-11de-9308-ab3afd9c0aba>
2009-08-17 20:40:00 +0000

    Tirar do classpath

--- JbossCacheCore/src/org/jboss/cache/RPCManagerImpl.java
+++
@@ -1,5 +0,0 @@
-package org.jboss.cache;
-
-public class RPCManagerImpl extends RPCManagerImpl_G2CL {
-
-}
--- JbossCachePojo/src/org/jboss/cache/pojo/eviction/AopEvictionPolicy.java
+++
@@ -1,8 +0,0 @@
-package org.jboss.cache.pojo.eviction;
-
-/**
- * Just a maker interface for PojoCache-specific eviction algorithm.
- */
-public interface AopEvictionPolicy
-{
-}
