dc9ab411a6e1b207ff3cbd3f061e9bf8da0d752d
mathcunha <mathcunha@81f30078-14c3-11de-9308-ab3afd9c0aba>
2010-02-25 19:34:26 +0000

    Storage - List/Show

--- Onaga/src/br/unifor/onaga/web/servlet/OCCIStorageServlet.java
+++ Onaga/src/br/unifor/onaga/web/servlet/OCCIStorageServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import br.unifor.onaga.occi.xml.Disk;
 import br.unifor.onaga.occi.xml.Storage;
 import br.unifor.onaga.web.OCCIClient;
 
@@ -16,37 +17,61 @@ import br.unifor.onaga.web.OCCIClient;
  * Servlet implementation class OCCIStorageServlet
  */
 public class OCCIStorageServlet extends HttpServlet {
-	protected Logger log = Logger.getLogger(OCCIStorageServlet.class.getName()); 
-	
+	protected Logger log = Logger.getLogger(OCCIStorageServlet.class.getName());
+
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public OCCIStorageServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public OCCIStorageServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		doRequest(request, response);
 	}
 
-	private void doRequest(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+	private void doRequest(HttpServletRequest request,
+			HttpServletResponse response) throws IOException, ServletException {
+		String acao = request.getParameter("acao");
+
+		if ("list".equals(acao)) {
+			list(request, response);
+		} else if ("show".equals(acao)) {
+			show(request, response);
+		}
+
+	}
+
+	private void list(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		Storage retorno = OCCIClient.list_storage();
-		
-		
 		request.setAttribute("storage", retorno);
-		request.getRequestDispatcher("/jsp/OCCI/storageList.jsp").forward(request, response);
+		request.getRequestDispatcher("/jsp/OCCI/storageList.jsp").forward(
+				request, response);
+	}
+
+	private void show(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		Disk retorno = OCCIClient.show_storage(request.getParameter("id"));
+		request.setAttribute("disk", retorno);
+		request.getRequestDispatcher("/jsp/OCCI/storageShow.jsp").forward(
+				request, response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		doRequest(request, response);
 	}
 
