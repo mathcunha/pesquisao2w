e96ba956054db71bb242c931450d0a91a0f97ae9
mathcunha <mathcunha@81f30078-14c3-11de-9308-ab3afd9c0aba>
2010-04-10 03:28:02 +0000

    cliente finalizado

---
+++ OnagaClient/appClientModule/Main_JNDI.java
@@ -0,0 +1,46 @@
+import javax.naming.Context;
+import javax.naming.InitialContext;
+
+import br.unifor.onaga.ejb.entity.VirtualAppliance;
+import br.unifor.onaga.ejb.entity.VirtualMachine;
+import br.unifor.onaga.ejb.session.RegisterSessionRemote;
+
+
+public class Main_JNDI {
+
+	private static final String JNDI_NAME = "br.unifor.onaga.ejb.session.RegisterSession"+ "_" + RegisterSessionRemote.class.getName() + "@Remote";
+	private static final String JNDI_NAME_PEQUENO = "RegisterSession";
+	
+	/**
+     * Utility class. No public constructor
+     */
+    private Main_JNDI() {
+    }
+
+    /**
+     * Main method.
+     * @param args the arguments (not required)
+     * @throws Exception if exception is found.
+     */
+    public static void main(final String[] args) throws Exception {
+        Context initialContext = new InitialContext();
+
+        RegisterSessionRemote businessItf =
+           (RegisterSessionRemote) initialContext.lookup(JNDI_NAME_PEQUENO);
+
+        VirtualAppliance appliance = new VirtualAppliance();
+        appliance.setName(args[0]);
+        appliance = (VirtualAppliance) businessItf.add(appliance);
+        System.out.println(appliance.toString());
+        
+        
+        VirtualMachine machine = new VirtualMachine();
+        machine.setName(args[1]);
+        machine.setIp(args[2]);
+        machine.setVirtualAppliance(appliance);
+        businessItf.add(machine);
+        
+    }
+
+
+}
--- OnagaEJB/ejbModule/br/unifor/onaga/ejb/session/RegisterSession.java
+++ OnagaEJB/ejbModule/br/unifor/onaga/ejb/session/RegisterSession.java
@@ -5,9 +5,13 @@ import javax.annotation.PreDestroy;
 import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
+import javax.ejb.TransactionManagement;
+import javax.ejb.TransactionManagementType;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import br.unifor.onaga.ejb.entity.OnagaEntityAB;
+import br.unifor.onaga.ejb.entity.VirtualAppliance;
 import br.unifor.onaga.ejb.entity.VirtualMachine;
 
 /**
@@ -15,12 +19,13 @@ import br.unifor.onaga.ejb.entity.VirtualMachine;
  */
 @Stateless(mappedName = "RegisterSession")
 @TransactionAttribute(TransactionAttributeType.SUPPORTS)
+@TransactionManagement(TransactionManagementType.CONTAINER)
 public class RegisterSession implements RegisterSessionRemote,
 		RegisterSessionLocal {
 
 	@PersistenceContext(unitName = "onaga")
-    private EntityManager em;
-	
+	private EntityManager em;
+
 	@PostConstruct
 	public void initialize() {
 
@@ -30,9 +35,32 @@ public class RegisterSession implements RegisterSessionRemote,
 	public void destroyBean() {
 
 	}
-	
+
 	@TransactionAttribute(TransactionAttributeType.REQUIRED)
-	public VirtualMachine addVirtualMachine(String name, String ip, String info){
+	public VirtualMachine addVirtualMachine(VirtualMachine virtualMachine) {
+
+		em.persist(virtualMachine);
+		System.out.println(virtualMachine.toString());
+		return virtualMachine;
+	}
+
+	@TransactionAttribute(TransactionAttributeType.REQUIRED)
+	public VirtualAppliance addVirtualAppliance(
+			VirtualAppliance virtualAppliance) {
+
+		em.persist(virtualAppliance);
+		System.out.println(virtualAppliance.toString());
+		return virtualAppliance;
+	}
+
+	@TransactionAttribute(TransactionAttributeType.REQUIRED)
+	public OnagaEntityAB add(OnagaEntityAB onagaEntity) {
+		if (onagaEntity instanceof VirtualMachine) {
+			return addVirtualMachine((VirtualMachine) onagaEntity);
+		} else if (onagaEntity instanceof VirtualAppliance) {
+			return addVirtualAppliance((VirtualAppliance) onagaEntity);
+		}
+		em.flush();
 		return null;
 	}
 }
